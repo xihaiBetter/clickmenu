b163147ab7f01dd450d3b2ec4a0d05c8a288b7e8
xi.xinhai <xi.xinhai@livecom.hk>
2015-06-05 08:53:47 +0800

    点击下拉菜单

---
+++ HWDropdownMenu.h
@@ -0,0 +1,31 @@
+//
+//  HWDropdownMenu.h
+//  黑马微博2期
+//
+//  Created by apple on 14-10-8.
+//  Copyright (c) 2014年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWDropdownMenu : UIView
++ (instancetype)menu;
+
+/**
+ *  显示
+ */
+- (void)showFrom:(UIView *)from;
+/**
+ *  销毁
+ */
+- (void)dismiss;
+
+/**
+ *  内容
+ */
+@property (nonatomic, strong) UIView *content;
+/**
+ *  内容控制器
+ */
+@property (nonatomic, strong) UIViewController *contentController;
+@end
